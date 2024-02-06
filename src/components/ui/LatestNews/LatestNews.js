f720e5f72936aaad95b2cab29f6ddbedca7b277b
Saif Maamun <sadifmamun@gmail.com>
2024-02-06 13:01:10 +0600

    full dynamic

--- src/components/ui/LatestNews/LatestNews.js
+++ src/components/ui/LatestNews/LatestNews.js
@@ -5,147 +5,83 @@ import gridImage2 from '../../../assets/latest3.png';
 import gridImage3 from '../../../assets/latest4.png';
 import gridImage4 from '../../../assets/latest5.png';
 import Image from 'next/image';
+import { getAllNews } from '@/utils/getAllNews';
 
-const LatestNews = () => {
+const LatestNews = async () => {
+    const { data } = await getAllNews()
+    console.log(data[0])
     return (
         <Box>
             <Card className='w-full'>
                 <CardActionArea>
                     <CardMedia>
 
-                        <Image src={latestImage} width='800' alt='latestNews' />
+                        <Image src={data[0].thumbnail_url} height={800} width='800' alt='latestNews' />
                     </CardMedia>
 
                     <CardContent>
                         <p className=
                             "bg-red-500 px-2 text-white my-5 rounded w-[100px]"
                         >
-                            Technology
+                            {data[0].category}
                         </p>
                         <Typography gutterBottom variant="h5" component="div">
-                            Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
+                            {data[0].title}
                         </Typography>
                         <Typography gutterBottom className='my-2'>
-                            By Awlad Hossain - Mar 18 2023
+                            By {data[0].author.name}   {data[0].author.published_date}
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout........
+                            {data[0].details.length > 200 ? data[0].details.slice(0, 200) + "..." : data[0].details}
                         </Typography>
                     </CardContent>
                 </CardActionArea>
             </Card>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={6}>
-                    <Card className='w-full my-4'>
-                        <CardActionArea>
-                            <CardMedia>
+            <Grid container rowSpacing={1} className='my-2' columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                {
 
-                                <Image src={gridImage1} width='800' alt='latestNews' />
-                            </CardMedia>
+                    data.slice(0, 4).map(news => (
+                        // console.log(news)
+                        <Grid key={news.id} item xs={6}>
+                            <Card className='my-2'>
+                                <CardActionArea>
+                                    <CardMedia sx={{
+                                        "& img": {
+                                            width: "500px",
+                                            height: "250px",
+                                        }
+                                    }}>
 
-                            <CardContent>
-                                <p className=
-                                    "bg-red-500 px-2 text-white my-5 rounded w-[100px]"
-                                >
-                                    Technology
-                                </p>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    EU’s New Sanctions Aim to Cut Russia Off From World Bank
-                                </Typography>
-                                <Typography gutterBottom variant="body2" className='my-2'>
-                                    By Saif Maamun - Feb 04 2024
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout........
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card className='w-full my-4'>
-                        <CardActionArea>
-                            <CardMedia>
+                                        <Image src={news.thumbnail_url} height={800} width='800' alt='latestNews' />
+                                    </CardMedia>
 
-                                <Image src={gridImage2} width='800' alt='latestNews' />
-                            </CardMedia>
+                                    <CardContent>
+                                        <p className=
+                                            "bg-red-500 px-2 text-white my-5 rounded w-[100px]"
+                                        >
+                                            {news.category}
+                                        </p>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {news.title.length > 35 ? news.title.slice(0, 35) + "..." : news.title}
+                                        </Typography>
+                                        <Typography gutterBottom className='my-2'>
+                                            By {news.author.name}   {news.author.published_date}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {news.details.length > 80 ? news.details.slice(0, 80) + "..." : news.details}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
 
-                            <CardContent>
-                                <p className=
-                                    "bg-red-500 px-2 text-white my-5 rounded w-[100px]"
-                                >
-                                    Technology
-                                </p>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Battle Analysis: Russian Tank Armada vs Ukraine
-                                </Typography>
-                                <Typography gutterBottom className='my-2'>
-                                    By Awlad Hossain - Mar 18 2023
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout........
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card className='w-full my-4'>
-                        <CardActionArea>
-                            <CardMedia>
+                        </Grid>
+                    ))
 
-                                <Image src={gridImage3} width='800' alt='latestNews' />
-                            </CardMedia>
+                }
 
-                            <CardContent>
-                                <p className=
-                                    "bg-red-500 px-2 text-white my-5 rounded w-[100px]"
-                                >
-                                    Technology
-                                </p>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    France’s Largest Business Summit to Conclude on Mar 22
-                                </Typography>
-                                <Typography gutterBottom className='my-2'>
-                                    By Awlad Hossain - Mar 18 2023
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout........
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card className='w-full my-4'>
-                        <CardActionArea>
-                            <CardMedia>
-
-                                <Image src={gridImage4} width='800' alt='latestNews' />
-                            </CardMedia>
-
-                            <CardContent>
-                                <p className=
-                                    "bg-red-500 px-2 text-white my-5 rounded w-[100px]"
-                                >
-                                    Technology
-                                </p>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Marquez Explains Lack of Confidence During Qatar GP
-                                </Typography>
-                                <Typography gutterBottom className='my-2'>
-                                    By Saif Maamun - Feb 04 2024
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout........
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
 
             </Grid>
-        </Box>
+        </Box >
     );
 };
 
